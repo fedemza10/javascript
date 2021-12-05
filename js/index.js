4daeae56403ac9984bb8f63da0fee1790d0288ac
Federico Rosales <federicorosalesmdz@gmail.com>
2021-12-05 20:31:30 -0300

    Javascript2

--- js/index.js
+++ js/index.js
@@ -1,50 +1,78 @@
-let nombre = prompt ("Cual es tu nombre?");
+const calcularMostrar = (recargo) => {
+  let total = cantidadP * recargo;
+  let valorCuota = total / cantidadCuotas;
+  alert("Total financiado $ " + total.toFixed());
+  alert(" Valor de cada cuota $ " + valorCuota.toFixed());
 
-   if ( !nombre /*|| nombre !== '' */ ) { alert ( 'Debe introducir un nombre');
-    console.log ( 'introducir nombre');
-   throw new Error ('obligatorio introducir nombre');}
+  console.log("Total $ ", total.toFixed());
+  for (let i = 1; i <= cantidadCuotas; i++) {
+    console.log("Cuota Nro:  " + i + " - $ " + valorCuota.toFixed(2));
+  }
+};
+let recargo3 = 1.1;
+let recargo6 = 1.2;
+let recargo12 = 1.4;
 
-   else {let cartel  = 'Bienvenido' + ' ' + nombre;
-    alert(cartel);
-    console.log (cartel);
-    console.log (nombre.toUpperCase(), typeof nombre)};
+let nombre = prompt("Cual es tu nombre?").toUpperCase();
 
+if (!nombre /*|| nombre !== '' */) {
+  alert("Debe introducir un nombre");
+  console.log("introducir nombre");
+  throw new Error("obligatorio introducir nombre");
+} else {
+  let cartel = "Bienvenido" + " " + nombre;
+  alert(cartel);
+  console.log(cartel);
+  console.log(nombre.toUpperCase(), typeof nombre);
+}
 
-let edad = prompt ('Cual es tu edad?');
-   if (  edad >= 18 ) { alert ('Acceso concedido');
-    console.log (edad,  typeof +edad);
-    console.log ( 'Acceso Concedido');
-    }
+let edad = prompt("Cual es tu edad?");
+if (edad >= 18) {
+  alert("Acceso concedido");
+  console.log(edad, typeof +edad);
+  console.log("Acceso Concedido");
+} else {
+  alert("Acceso Denegado - Debes ser mayor de 18 años");
+  console.log("Acceso Denegado");
+  throw new Error("Debes ser mayor de 18 años de edad");
+}
 
-   else { alert ('Acceso Denegado - Debes ser mayor de 18 años');
-    console.log ( 'Acceso Denegado');
-    throw new Error ( 'Debes ser mayor de 18 años de edad');}
+let prestamo = +prompt(
+  "Desea solicitar un préstamo para empleados? 1-SI  2-NO"
+);
+if (prestamo != 1) {
+  alert(" Hasta luego");
+  console.log("Hasta Luego");
+  throw new Error("Cortar ejecución, no desea el producto");
+} else prestamo == 1;
+{
+  alert(" Sigamos adelante");
+  console.log("Sigamos adelante");
+}
 
-let prestamo = +prompt ('Desea solicitar un préstamo para empleados? 1-SI  2-NO');
-   if (prestamo != 1) {alert (' Hasta luego');
-     console.log ('Hasta Luego');
-     throw new Error ('Cortar ejecución, no desea el producto');
-      }
+let cantidadP = +prompt(
+  "Según tu score, sólo puedes tomar hasta $50.000 en  3, 6 0 12 cuotas con interes."
+);
+if (cantidadP > 50000) {
+  alert("Excede límite. Hasta luego");
+  console.log("No acepto la propuesta de prestamo");
+  throw new Error("Cortar ejecucion, no acepto la propuesta");
+} else cantidadP >= 10000 || cantidadP <= 50000;
+{
+  alert(" A continuación, debes seleccionar la cantidad de cuotas");
+  console.log("aceptó y debe elegir cuotas");
+}
 
-   else (prestamo ==1);  { alert (' Sigamos adelante');
-     console.log ('Sigamos adelante');
-
-     confirmarPrestamo = +prompt  
-           ('Según tu score sólo podes tomar $50.000 en  hasta 6 cuotas con un interés total del 10%. 1-SI  2-NO');
-            if (confirmarPrestamo !=1) {alert ('Hasta luego');
-             console.log ('No acepto la propuesta de prestamo')
-            throw new Error ( 'Cortar ejecucion, no acepto la propuesta');
-               }
-            else (confirmarPrestamo ==1); {alert(' El siguiente es tu esquema de cuotas a pagar a partir del 02/01/2022');
-                console.log (' Aceptó el préstamo');
-                const suma = (a,b) => a + b;
-                const div = (a,b) => a / b;
-                const interes = x => x * 0.10;
-                let capital =  50000 ;
-                let cuotas = 6;
-                let valorCuota = div ( suma (capital, interes(capital)), cuotas);
-                alert ( valorCuota =  '6 cuotas ' + '  de ' + ' $ ' + valorCuota );
-                console.log (valorCuota = valorCuota + ' ' + 'X ' + '6 cuotas');
-               } }
-
-
+let cantidadCuotas = +prompt(" Seleccionar 3, 6 o 12 cuotas");
+console.log(" Elige cuotas");
+if (cantidadCuotas == 3) {
+  calcularMostrar(recargo3)
+} else if (cantidadCuotas == 6) {
+  calcularMostrar(recargo6);
+} else if (cantidadCuotas == 12) {
+  calcularMostrar(recargo12);
+} else {
+  alert (' No se ingresó un valor correcto');
+  console.log("No se ingreso valor correcto.");
+  throw new Error ('El valor ingresado es invalido');
+}
