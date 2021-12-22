a8fb315ec6d4919a4b5246994bcbcdc695360e22
Federico Rosales <federicorosalesmdz@gmail.com>
2021-12-22 20:03:22 -0300

    Javascript Eventos y 2da entrega

--- js/index.js
+++ js/index.js
@@ -12,35 +12,59 @@ const carrito1 = new Carrito();
 carrito1.createItem(alimentoPerro2);
 const alimentoPerro5 = new AlimentoPerro(20, "proplan", 50, 5000, true);
 carrito1.createItem(alimentoPerro5);
-console.log (carrito1.findAllItems());
+console.log(carrito1.findAllItems());
+
+// Captura evento "mouseover y mouseleave"
+let mouse = document.getElementById("productos-lista");
+mouse.onmouseover = () => {
+  mouse.style.backgroundColor = "yellow";
+};
+mouse.onmouseleave = () => {
+  mouse.style.backgroundColor = "white";
+};
+
+// Captura Evento sobre botón "Iniciar carga de datos, emite alert y console.log"
+let btn1 = document.getElementById("btn1");
+btn1.onclick = () => {
+  alert(
+    " Al completar este formulario, el producto será ingresado al storage."
+  );
+  console.log(' Esto es una prueba del evento "onclick"');
+};
 
 // Esto viene del storage
 
 const productos = carrito1.findAllItems();
 
- //Formulario para obtener datos
+//Formulario para obtener datos
 
-const formProducto = document.getElementById('form-producto')
-const inputIdProducto = document.getElementById('input-id-producto')
-const inputNombreProducto = document.getElementById('input-nombre-producto')
-const inputCantidadProducto = document.getElementById('input-cantidad-producto')
-const inputPrecioProducto = document.getElementById('input-precio-producto')
+const formProducto = document.getElementById("form-producto");
+const inputIdProducto = document.getElementById("input-id-producto");
+const inputNombreProducto = document.getElementById("input-nombre-producto");
 
-formProducto.addEventListener('submit', (event) => {
-  event.preventDefault()
-  const id = +inputIdProducto.value
-  const nombre = inputNombreProducto.value  
-  const cantidad = +inputCantidadProducto.value
-  const precio = +inputPrecioProducto.value
+// Captura Evento sobre input "Iniciar carga de datos, emite alert y console.log"
+inputNombreProducto.onchange = () => {
+  console.log();
+};
 
-  const alimentoN = new AlimentoPerro (id, nombre, cantidad, precio, true)
+const inputCantidadProducto = document.getElementById(
+  "input-cantidad-producto"
+);
+const inputPrecioProducto = document.getElementById("input-precio-producto");
 
-  carrito1.createItem(alimentoN)
+// Captura evento Submit y ejecuta función
+formProducto.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const id = +inputIdProducto.value;
+  const nombre = inputNombreProducto.value;
+  const cantidad = +inputCantidadProducto.value;
+  const precio = +inputPrecioProducto.value;
+
+  const alimentoN = new AlimentoPerro(id, nombre, cantidad, precio, true);
+
+  carrito1.createItem(alimentoN);
   formProducto.reset();
-  console.log (carrito1)
-})
+  console.log(carrito1);
+});
 
-
-renderList ('productos-lista', productos)
-
-
+renderList("productos-lista", productos);
